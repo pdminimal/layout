82f3616e26e7eeb9294e8f4a7a0ff8368ed93054
pdminimal <pdminimal@gmail.com>
2022-07-18 13:55:09 +0900

    Setup rome formatter

--- frontend/src/test/main.test.ts
+++ frontend/src/test/main.test.ts
@@ -1,13 +1,7 @@
 import { describe, expect, it } from "vitest";
 
-describe(
-  "main",
-  () => {
-    it(
-      "should be true",
-      () => {
-        expect([2, [3]]).toEqual([2, [3]]);
-      },
-    );
-  },
-);
+describe("main", () => {
+  it("should be true", () => {
+    expect([2, [3]]).toEqual([2, [3]]);
+  });
+});
